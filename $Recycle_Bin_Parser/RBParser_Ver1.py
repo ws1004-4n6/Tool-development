1655e44e93a19bc0b0286dca7e073564bb46249b
ws1004 <63801211+ws1004-4n6@users.noreply.github.com>
2020-10-25 15:55:27 +0900

    Add files via upload

---
+++ $Recycle_Bin_Parser/RBParser_Ver1.py
@@ -0,0 +1,162 @@
+from datetime import datetime, timedelta
+import struct
+import sys, string
+
+def convert_dword(fbytes):          # Four Byte to int
+    return struct.unpack_from("I", fbytes)[0]
+
+def convert_dwordlong(ebytes):      # Eight Byte to int
+    return struct.unpack_from("Q", ebytes)[0]
+
+def convert_string(nbytes, data):   # NByte to String
+    thestring = struct.unpack_from("{}s".format(str(nbytes)), data)[0]
+    thestring = thestring.decode('ascii', 'ignore')
+    return thestring
+
+def convert_timestamp(time):        # convert timestamp
+    return str(datetime(1600,1,1) + timedelta(microseconds=time / 10.))
+
+class recycle_bin_winXP(object):
+    def __init__(self, FILE):
+        self.filedata = self.FileData(FILE=FILE)
+        
+    def FileData(self, FILE):
+        self.version = str(convert_dword(FILE[0:4]))+" (Windows XP)"
+        self.info2_filesize = len(FILE)
+        self.entry_size = convert_dword(FILE[12:16])
+
+        result = []
+        offset = 0
+        F = FILE[offset:offset+self.entry_size]
+        for i in range(0, self.info2_filesize//self.entry_size):
+            data = []
+            if F[20] == 0x00:        # 휴지통에서 삭제된 파일
+                self.remain = "No"
+            else:                           # 휴지통에 파일이 남아있음.
+                self.remain = "YES"
+            data.append(convert_dword(F[280:284]))                              # Index
+            data.append(convert_timestamp(convert_dwordlong(F[288:296])))       # Deleted Time
+            data.append(self.remain)                                            # Remain in RB
+            data.append(convert_dword(F[296:300]))                              # File Size
+            
+            strings_data = convert_string(len(F[300::]), F[300::])
+            data.append(strings_data.replace("\x00",""))
+            result.append(data)
+            offset = offset + self.entry_size
+            F = FILE[offset:offset+self.entry_size]
+
+        self.path_len = 0
+        for i in range(0, (self.info2_filesize//self.entry_size)-1):
+                if self.path_len <= len(result[i][4]):
+                    self.path_len = len(result[i][4])
+
+        return result
+    
+class recycle_bin_win78(object):
+    def __init__(self, FILE):
+        self.FileHeader(FILE=FILE)
+        self.deleted_file_name = self.FileName(FILE=FILE, offset=24, length=520)
+
+    def FileHeader(self, FILE):
+        F = FILE[0:28]
+        self.version = str(convert_dwordlong(F[0:8]))+" (Windows 78)"
+        self.deleted_file_size = str(convert_dwordlong(F[8:16]))+" Byte"
+        self.deleted_file_time = convert_timestamp(convert_dwordlong(F[16:24]))
+
+    def FileName(self, FILE, offset, length):
+        strings = ""
+        F = FILE[offset:offset+length]
+        strings_data = convert_string(length, F)
+        strings += strings_data.replace("\x00","")
+
+        return strings
+
+class recycle_bin_win10(object):
+    def __init__(self, FILE):
+        self.FileHeader(FILE=FILE)
+        self.deleted_file_name = self.FileName(FILE=FILE, offset=28, length=self.deleted_file_name_size)
+
+    def FileHeader(self, FILE):
+        F = FILE[0:28]
+        self.version = str(convert_dwordlong(F[0:8]))+" (Windows 10)"
+        self.deleted_file_size = str(convert_dwordlong(F[8:16]))+" Byte"
+        self.deleted_file_time = convert_timestamp(convert_dwordlong(F[16:24]))
+        self.deleted_file_name_size = convert_dword(F[24:28])*2
+
+    def FileName(self, FILE, offset, length):
+        strings = ""
+        F = FILE[offset:offset+length]
+        strings_data = convert_string(length, F)
+        strings += strings_data.replace("\x00","")
+
+        return strings
+
+def print_data(FILE):
+    with open(FILE, "rb") as f:
+        data = f.read()
+        version = convert_dwordlong(data[0:8])
+        if version == 1:            # windows 7 or windows 8.1
+            r = recycle_bin_win78(bytearray(data))
+            print("Command Line : python {} {}\n".format(sys.argv[0],sys.argv[1]))        
+            print("Source File : {}\n".format(sys.argv[1]))
+            
+            if len(r.deleted_file_name) > len(r.deleted_file_time):
+                print("┌"+"───────────────────"+"─"*(len(r.deleted_file_name))+"─┐")
+                print("│ Version: {}".format(r.version)+" "*(len(r.deleted_file_name)-4)+"│")
+                print("│ Deleted File Size: {}".format(r.deleted_file_size)+" "*(len(r.deleted_file_name)-len(r.deleted_file_size))+"│")
+                print("│ Deleted File Name: {}│".format(r.deleted_file_name))
+                print("│ Deleted File Time: {}".format(r.deleted_file_time)+" "*(len(r.deleted_file_name)-len(r.deleted_file_time))+"│")
+                print("└"+"───────────────────"+"─"*(len(r.deleted_file_name))+"─┘")
+            else:
+                print("┌"+"───────────────────"+"─"*(len(r.deleted_file_time))+"─┐")
+                print("│ Version: {}".format(r.version)+" "*(len(r.deleted_file_time)-4)+"│")
+                print("│ Deleted File Size: {}".format(r.deleted_file_size)+" "*(len(r.deleted_file_time)-len(r.deleted_file_size))+"│")
+                print("│ Deleted File Name: {}".format(r.deleted_file_name)+" "*(len(r.deleted_file_time)-len(r.deleted_file_name))+"│")
+                print("│ Deleted File Time: {}".format(r.deleted_file_time)+" "*(len(r.deleted_file_name)-len(r.deleted_file_time))+"│")
+                print("└"+"───────────────────"+"─"*(len(r.deleted_file_time))+"─┘")
+        elif version == 2:          # windows 10
+            r = recycle_bin_win10(bytearray(data))
+            print("Command Line : python {} {}\n".format(sys.argv[0],sys.argv[1]))        
+            print("Source File : {}\n".format(sys.argv[1]))
+            
+            if len(r.deleted_file_name) > len(r.deleted_file_time):
+                print("┌"+"───────────────────"+"─"*(len(r.deleted_file_name))+"─┐")
+                print("│ Version: {}".format(r.version)+" "*(len(r.deleted_file_name)-4)+"│")
+                print("│ Deleted File Size: {}".format(r.deleted_file_size)+" "*(len(r.deleted_file_name)-len(r.deleted_file_size))+"│")
+                print("│ Deleted File Name: {}│".format(r.deleted_file_name))
+                print("│ Deleted File Time: {}".format(r.deleted_file_time)+" "*(len(r.deleted_file_name)-len(r.deleted_file_time))+"│")
+                print("└"+"───────────────────"+"─"*(len(r.deleted_file_name))+"─┘")
+            else:
+                print("┌"+"───────────────────"+"─"*(len(r.deleted_file_time))+"─┐")
+                print("│ Version: {}".format(r.version)+" "*(len(r.deleted_file_time)-4)+"│")
+                print("│ Deleted File Size: {}".format(r.deleted_file_size)+" "*(len(r.deleted_file_time)-len(r.deleted_file_size))+"│")
+                print("│ Deleted File Name: {}".format(r.deleted_file_name)+" "*(len(r.deleted_file_time)-len(r.deleted_file_name))+"│")
+                print("│ Deleted File Time: {}".format(r.deleted_file_time)+" "*(len(r.deleted_file_name)-len(r.deleted_file_time))+"│")
+                print("└"+"───────────────────"+"─"*(len(r.deleted_file_time))+"─┘")
+        else:                       # windows XP
+            r = recycle_bin_winXP(bytearray(data))
+            print("Command Line : python {} {}\n".format(sys.argv[0],sys.argv[1]))        
+            print("Source File : {}\n".format(sys.argv[1]))
+            print("Version: {}".format(r.version))
+            print("Time zone: Coordinated Universal Time [UTC+0]\n")
+            
+            print("┌"+"─────────────────────────────────────────────────────────────────"+"─"*(r.path_len)+"─┐")
+            print("│ Index | Deleted Time               | Remain in RB | File Size | Path"+" "*(r.path_len-4)+" │")
+            for data in r.filedata:
+                print("│ {0:<5} | {1:<26} | {2:<12} | {3:<9} | {4:<{5}} │".format(data[0],data[1],data[2],data[3],data[4],r.path_len))
+            print("└"+"─────────────────────────────────────────────────────────────────"+"─"*(r.path_len)+"─┘")
+
+def main():
+    print("RBParser version 1.0.0\n")
+    print("Author: ws1004 (ws1004@kakao.com)")
+    print("https://github.com/ws1004-4n6/Tool-development/RBParser\n")
+    if sys.version_info > (3, 8, 0):
+        if len(sys.argv) != 2:
+            sys.exit("Example : python {0} [INFO2 FILE]\n          python {0} [$I###### FILE]".format(sys.argv[0]))
+        else:
+            print_data(sys.argv[1])
+    else:
+        sys.exit("Using Python Version 3.8.0 or later to run this script")
+
+if __name__ == "__main__":
+    main()
---
+++ $Recycle_Bin_Parser/README.md
@@ -0,0 +1,93 @@
+<h1>Windows Recycle Bin Parser Version.1</h1>
+
+<h2>Korean Description</h2>
+
+<h3>설명</h3>
+
+Recycle Bin 아티팩트 파일은 Windows 에서 휴지통이라고 불리는 아티팩트 이며 삭제된 파일을 저장하는 공간으로 사용된다.
+RBParser_Ver1.py는 Windows XP, Windows 7, Windows 8.1, Windows 10의 OS에서 지원하는 모든 Recycle Bin 아티팩트 파일을 분석할 수 있습니다.
+
+<h3>사용방법</h3>
+
+Recycle Bin 파일 하나에 대한 툴 사용법
+
+```
+python RBParser_Ver1.py [Recycle_Bin_File]
+```
+
+디렉터리 하위에 있는 모든 .pf 파일에 대한 툴 사용법
+
+```
+python RBParser_Ver1.py [INFO2 FILE]
+
+or
+
+python RBParser_Ver1.py [$I###### FILE]
+```
+
+<h3>다른 툴과 비교했을때 개선된점</h3>
+
+- Windows XP , Windows 7, Windows 8 , Windows 10의 Recycle Bin 을 하나의 도구로 분석해 주는 도구가 없다.
+- 출력된 데이터를 좀더 깔끔하게 출력해준다.
+
+<h3>부족한 부분</h3>
+
+- Windows 7,8,10 에서는 $I##### 파일 1개를 INPUT 하여 데이터를 출력해 주지만 휴지통 자체의 파일을 전부 읽어서 데이터를 리스트화 해주지 않음.
+- CSV 파일로 내보내기 기능
+- 한글과 같은 유니코드 출력 기능
+
+위의 기능들은 RBParser_Ver2.py에서 다룰 예정입니다.
+
+<h3>파이썬 요구 모듈</h3>
+
+- from datetime import datetime, timedelta
+- import struct
+- import sys
+- import string
+
+---
+
+<h2>English Description</h2>
+
+<h3>Description</h3>
+
+Recycle Bin artifact files are artifacts called trash cans in Windows and are used as storage spaces for deleted files.
+RBParser_Ver1.py can analyze any Recycle Bin artifact file supported by OS in Windows XP, Windows 7, Windows 8.1, and Windows 10.
+
+<h3>How To Use?</h3>
+
+How to use tools for Recycle Bin files
+
+```
+python RBParser_Ver1.py [Recycle_Bin_File]
+```
+
+Using the tool for all ".pf" files in the lower part of the directory
+
+```
+python RBParser_Ver1.py [INFO2 FILE]
+
+or
+
+python RBParser_Ver1.py [$I###### FILE]
+```
+
+<h3>Improvements compared to other tools</h3>
+
+- There is no tool to analyze Recycle Bin on Windows XP, Windows 7, Windows 8, or Windows 10 with one tool.
+- The printed data should be printed more neatly.
+
+<h3>Deficient Part</h3>
+
+- Windows 7,8,10 will print out one $I#### file, but will not list the data by reading all of the files in the trash.
+- Export function to CSV file
+- Unicode output functions such as Hangul
+
+The above functions will be covered by RBParser_Ver2.py
+
+<h3>Python Requirements Module</h3>
+
+- from datetime import datetime, timedelta
+- import struct
+- import sys
+- import string
